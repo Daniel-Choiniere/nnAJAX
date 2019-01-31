87e28a107f5525a9f6db0624bca3a3ea2f29da16
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-31 03:58:20 +0000

    synchrnous and asynchronous callback functions

--- async.js
+++ async.js
@@ -1,20 +1,25 @@
-window.onload = function(){
+/*global $*/
+window.onload = function() {
+
+    //SYNCCHRNOUS CALLBACK FUNCTION 
+    // function callback(val){
+    //        console.log(val);
+    //    }
+
+    //    let fruits = ["bannana", "apple", "pear"];
+    //    fruits.forEach(callback);
+
+    // ASYNCHRONOUS CALLBACK FUNCTION
+//     $.get("tweets.json", function(data) {
+//         console.log(data);
+//     });
+
+//     // or //
     
-    // vanilla javaScript method
-  let http = new XMLHttpRequest();  
-  
-  http.onreadystatechange = function() {
-      if(http.readyState == 4 && http.status == 200){
-        //   console.log(JSON.parse(http.response));
-      }
-  };
-  
-  http.open("GET", "tweets.json", true);
-  http.send();
-  
-//   Jquery method
-    $.get("tweets.json", function(data){
-        console.log(data);
-    });
+//     function cb(data){
+//         console.log(data);
+//     }
     
+//     $.get("tweets.json", cb);
+//         console.log("test");
 };
